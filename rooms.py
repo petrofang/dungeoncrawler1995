afac3586ea0b62b6bce3518f5417285c4ad36683
Giles <giles.cooper@gmail.com>
2025-01-24 20:18:12 -0500

    Add Room and Exit models for room management

---
+++ rooms.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+from typing import List
+
+from base import Base, pk_id, fk_from_room, fk_to_room
+
+class Room(Base):
+    __tablename__ = 'rooms'
+    id: Mapped[pk_id]
+    title: Mapped[str]
+    description: Mapped[str]
+
+    exits: Mapped[List["Exit"]] = relationship(back_populates="from_room", foreign_keys='Exit.from_room_id')
+    entries: Mapped[List["Exit"]] = relationship(back_populates="to_room", foreign_keys='Exit.to_room_id')
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}(id:{self.id})[{self.title}]>'
+
+class Exit(Base):
+    __tablename__ = 'exits'
+    id: Mapped[pk_id]
+    direction: Mapped[str] 
+    from_room_id: Mapped[fk_from_room]
+    to_room_id: Mapped[fk_to_room]
+
+    from_room: Mapped[Room] = relationship(back_populates='exits', foreign_keys='Exit.from_room_id')
+    to_room: Mapped[Room] = relationship(back_populates='entries', foreign_keys='Exit.to_room_id')
+
+    def  __repr__(self):
+        return f'<{self.__class__.__name__}(id:{self.id})[{self.from_room_id}]->{self.direction}->[{self.to_room_id}]>'
