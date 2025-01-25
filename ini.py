ce3967a3c6425b233c8a367e030429ec49fe7637
Giles <giles.cooper@gmail.com>
2025-01-25 16:35:40 -0500

    Move metadata to ini.py

--- ini.py
+++ ini.py
@@ -2,17 +2,21 @@
 STARTING_ROOM = 0
 
 
+__version__ = '0.1.0'
+__author__ = 'Giles Cooper'
 
-'''
+
+r'''
 Reference: ASCII Box Drawing Characters
 ═   ║   ╬   ╔   ╗   ╚   ╝   ╠   ╣   ╦   ╩
         ╪   ╒   ╕   ╘   ╛   ╞   ╡   ╤   ╧
         ╫   ╓   ╖   ╙   ╜   ╟   ╢   ╥   ╨
 ─   │   ┼   ┌   ┐   └   ┘   ├   ┤   ┬   ┴
-╌   ╎
+╌   ╎   			        ╶   ╴   ╷   ╵
 ┄   ┆
 ┈   ┊
 ╭   ╮
 ╰   ╯
+╱	╲	╳	
 ►   ◄   ▲   ▼
 '''
