4ae1529f8eff7c4e2b897e613dd82af156f83cad
Giles <giles.cooper@gmail.com>
2025-01-25 13:50:29 -0500

    Add ASCII box drawing characters reference to configuration file

--- ini.py
+++ ini.py
@@ -1,2 +1,18 @@
 # Description: This file contains the configuration for the game.
 STARTING_ROOM = 0
+
+
+
+'''
+Reference: ASCII Box Drawing Characters
+═   ║   ╬   ╔   ╗   ╚   ╝   ╠   ╣   ╦   ╩
+        ╪   ╒   ╕   ╘   ╛   ╞   ╡   ╤   ╧
+        ╫   ╓   ╖   ╙   ╜   ╟   ╢   ╥   ╨
+─   │   ┼   ┌   ┐   └   ┘   ├   ┤   ┬   ┴
+╌   ╎
+┄   ┆
+┈   ┊
+╭   ╮
+╰   ╯
+►   ◄   ▲   ▼
+'''
