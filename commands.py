69a0347f523eb1761326b73ab5b5222922e15dc9
Giles <giles.cooper@gmail.com>
2025-01-26 14:21:37 -0500

    Move targetting function to parser module

--- commands.py
+++ commands.py
@@ -158,90 +158,6 @@ class CommandList():
 #        actions.do(subject, action, dir)
 
 
-
-
-def find_target(player:Player, arg:str, type=None, in_room=False, in_inventory=False,
-                in_equipment=False, in_exits=False, in_containers=False, **kwargs):
-    """
-    This function searches for a target item, mobile, or exit by keyword 
-    and returns the game object if found, or None. 
-        arguments:
-            player - the player with whom the search is associated
-            arg - the keyword or name of the item being searched
-            type - Object, Mobile, or None, where None means either/both
-            in_room - search in the player's room. If type=Mobile, the
-                room will be searched even if in_room=False
-            in_inventory - search in the player's inventory.
-            in_equipment - search in the player's equipment.
-            in_containers - search in containers in myself's room and inventory.
-            in_exits - search for exits in the player's room.
-    """
-    # TODO: fuzzywuzzy? .like()?
-    target = None
-    if type is Creature or type is None: # not an object
-        for mobile in player.room.mobiles:
-            if mobile.name.startswith(arg) and mobile is not player: 
-                target = mobile
-            else:
-                names=mobile.name.split()
-                for name in names:
-                    if name.startswith(arg):
-                        target = mobile
-        if target: return target
-    if type is Item or type is None: # and if it's not a mobile it must be an object or None
-        if in_room:
-            for item in player.room.inventory:
-                if item.name.startswith(arg):    
-                    target = item
-                else:
-                    names=item.name.split()
-                    for name in names:
-                        if name.startswith(arg):
-                            target = item
-            if target: return target            
-        if in_inventory:
-            for item in player.inventory:
-                if item.name.startswith(arg): 
-                    target = item
-                else:
-                    names=item.name.split()
-                    for name in names:
-                        if name.startswith(arg):
-                            target = item
-            if target: return target
-        if in_equipment:
-            for item in [item for item in player.equipment.values() if item is not None]:
-                if item.name.startswith(arg): 
-                    target = item
-                else:
-                    names=item.name.split()
-                    for name in names:
-                        if name.startswith(arg):
-                            target = item
-            if target: return target
-        if in_containers:
-            for item in player.room.inventory:
-                if item.contents:
-                    for thing in item.contents:
-                        if thing.name.startswith(arg):
-                            target=thing
-            if target: return target
-            for item in player.inventory:
-                if item.contents:
-                    for thing in item.contents:
-                        if thing.name.startswith(arg):
-                            target=thing
-            if target: return 
-
-    if type is None:                        
-        if in_exits:
-            for exit in player.room.exits:
-                if exit.direction == arg:
-                    target = exit
-        return target
-    return target
-
-
 class AliasList():
     ''' 
     This is the master list of all player command aliases.
@@ -305,6 +221,7 @@ class AliasList():
         CommandList.go(player, 'out')
     
     d=down
+    u=up
     n=north
     ne=northeast
     e=east
