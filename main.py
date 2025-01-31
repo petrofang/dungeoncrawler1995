e5dc45e0797a9ca7856eab8120427ab605c95c0b
Giles <giles.cooper@gmail.com>
2025-01-30 20:32:23 -0500

    initiate action_thread to execute action queue

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import threading
+
 from engine import ConsoleIO, Parser
 import ini
 
-connection = ConsoleIO(Parser())
-connection.print(ini.splash_art)
-connection.start_session()
+def main():
+    from orm import process_actions
 
+    # Start the action processing thread
+    action_thread = threading.Thread(target=process_actions, daemon=True)
+    action_thread.start()
+
+    # Start the player connection thread:
+    connection = ConsoleIO(Parser())
+    connection.print(ini.splash_art)
+    connection.start_session()
+
+if __name__ == "__main__":
+    main()  
