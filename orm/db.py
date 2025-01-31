9c5565e8121d8091261dd40dd1be7cec4ab56012
Giles <giles.cooper@gmail.com>
2025-01-30 22:16:30 -0500

    add echo action and try/except to keep action processor from crashing

--- orm/db.py
+++ orm/db.py
@@ -5,6 +5,8 @@ import queue
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import Session, scoped_session
 
+from orm import Player
+
 # Load and validate database configuration
 load_dotenv()
 SERVER = os.getenv('DB_SERVER')
@@ -32,21 +34,31 @@ class Action():
     def chown(subject, target, arg, **kwargs):
         ''' Change the owner of an object. '''
         print(f"Changing owner of {target} to {arg}")
-        SQL.expire_all()
         target.owner = arg
         SQL.add(target)
-        SQL.commit()
         return True
+    
+    @staticmethod
+    def echo(subject, target, arg, **kwargs):
+        ''' Print to all in the player's room. '''
+        room = subject.owner
+        print(f"Echoing '{arg}' to room {repr(room)}")
+        for everyone in room.inventory:
+            if isinstance(everyone, Player):
+                everyone.io.print(arg)
 
 def do(subject, action, target, arg, **kwargs):
     action_queue.put((subject, action, target, arg, kwargs))
 
 def process_actions():
     while True:
-        subject, action, target, arg, kwargs = action_queue.get()
-        if action in Action.__dict__:
-            Action.__dict__[action](subject, target, arg, **kwargs)
-            SQL.commit()
-        else:
-            print(f"Unknown action: {action}")
-        action_queue.task_done()
+        try:
+            subject, action, target, arg, kwargs = action_queue.get()
+            if action in Action.__dict__:
+                Action.__dict__[action](subject, target, arg, **kwargs)
+                SQL.commit()
+            else:
+                print(f"Unknown action: {action}")
+            action_queue.task_done()
+        except Exception as e:
+            print(f"Error processing action:\n   {e}")
